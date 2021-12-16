3d1615dee4e307c0633e2c6556c53d33502c538f
Xavientois <joshua.rampersad@hotmail.com>
2021-12-16 08:59:21 -0500

    feat: simplify example to use no types

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,31 +1,31 @@
+use anyhow::Result;
 use promptly::prompt;
 use reqwest::{blocking, header};
-use std::error;
-use voiceflow::*;
+use serde_json::{json, Value as JsonValue};
 
 const API_KEY: &str = "YOUR_API_KEY_HERE"; // it should look like this: VF.XXXXXXX.XXXXXX... keep this a secret!
 const VERSION_ID: &str = "YOUR_VERSION_ID_HERE"; // your Voiceflow project versionID, find it under the "integrations" tab
 
 const API_URL: &str = "https://general-runtime.voiceflow.com";
 
-fn interact<R: Request>(user_id: &str, request: R) -> Result<bool, Box<dyn error::Error>> {
+fn interact(user_id: &str, request: JsonValue) -> Result<bool> {
     let url = format!("{}/state/{}/user/{}/interact", API_URL, VERSION_ID, user_id);
 
     // call the Voiceflow API with the user's name & request, get back a response
     let client = blocking::Client::new();
     let response = client
         .post(url)
-        .json(&request.body())
+        .json(&request)
         .header(header::AUTHORIZATION, API_KEY)
         .send()
         .and_then(blocking::Response::error_for_status)?;
 
-    let traces: Vec<Trace> = response.json()?;
+    let traces: Vec<JsonValue> = response.json()?;
 
     for trace in traces {
-        match trace.r#type.as_str() {
-            "text" | "speak" => println!("{}", trace.message()?),
-            "end" => return Ok(false),
+        match trace["type"].to_string().as_str() {
+            "\"text\"" | "\"speak\"" => println!("{}", trace["payload"]["message"]),
+            "\"end\"" => return Ok(false),
             _ => {}
         }
     }
@@ -33,15 +33,18 @@ fn interact<R: Request>(user_id: &str, request: R) -> Result<bool, Box<dyn error
     Ok(true)
 }
 
-fn main() -> Result<(), Box<dyn error::Error>> {
+fn main() -> Result<()> {
     let user_id: String = prompt("> What is your name?")?;
     // send a simple launch request starting the dialog
-    let mut is_running = interact(&user_id, LaunchRequest)?;
+    let mut is_running = interact(&user_id, json!({"request": {"type": "launch"}}))?;
 
     while is_running {
-        let next_input = prompt("> Say something")?;
+        let next_input: String = prompt("> Say something")?;
         // send a simple text type request with the user input
-        is_running = interact(&user_id, TextRequest::new(next_input))?;
+        is_running = interact(
+            &user_id,
+            json!({"request": {"type": "text", "payload": next_input}}),
+        )?;
     }
 
     println!("The end!");
